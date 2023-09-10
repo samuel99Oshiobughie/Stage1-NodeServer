11b9fd5212bcfe937f0ead01b13deb9e9d3a859a
samuel <samuel@samuels-MacBook-Pro.local>
2023-09-10 22:33:37 +0100

    First commit for stage 1 task

---
+++ stage1.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const stage1 = express();
+
+
+stage1.get('/api', (req, res) => {
+    const { slack_name, track } = req.query;
+    const currentDay = new Date().toLocaleDateString('en-US', { weekday: 'long'});
+    const utcTime = new Date().toISOString();
+    const githubFileUrl = 'https://github.com/username/repo/blob/main/file_name.ext';
+    const githubRepoUrl = 'https://github.com/username/repo';
+
+    const response = {
+        slack_name,
+        current_day: currentDay,
+        utc_time: utcTime,
+        track: track,
+        github_file_url: githubFileUrl,
+        github_repo_url: githubRepoUrl,
+        status_code: 200,
+      };
+
+      res.json(response);
+})
+
+const PORT = process.env.PORT || 3000;
+
+stage1.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
